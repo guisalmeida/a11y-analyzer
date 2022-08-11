4781633e0f48ee4c6ede396ad48c885da805bbc3
Guilherme Almeida <guisalmeida.dev@gmail.com>
2022-08-11 14:17:39 -0300

    test: add more examples with config

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,27 +1,37 @@
 /// <reference types="cypress" />
 
-describe('Todo app', () => {  
+describe('Example tests', () => {  
   it('Should log any accessibility failures', () => {
     cy.analyseA11y('https://example.cypress.io');
   })
 
-  // it('Should exclude specific elements on the page', () => {
-  //   cy.analyseA11y('https://example.cypress.io', { exclude: ['.learn'] });
-  // })
+  it('Should execute ONLY specific elements on the page', () => {
+    cy.analyseA11y('https://example.cypress.io', '.container', null);
+  })
+  
+  it('Should exclude specific elements on the page', () => {
+    cy.analyseA11y('https://example.cypress.io', { exclude: ['.banner'] }, null);
+  })
 
-  // it('Should execute only specific elements on the page', () => {
-  //   cy.analyseA11y('https://example.cypress.io', '.learn');
-  // })
+  it('Should ONLY include rules with serious and critical impacts', () => {
+    cy.analyseA11y('https://example.cypress.io', null, { includedImpacts: ['critical', 'serious'] });
+  })
 
-  // it('Should only include rules with serious and critical impacts', () => {
-  //   cy.analyseA11y('https://example.cypress.io', { includedImpacts: ['critical', 'serious'] });
-  // })
+  it('Should exclude specific accessibility rules', () => {
+    cy.analyseA11y('https://example.cypress.io', null, {
+      rules: {
+        'color-contrast': { enabled: false }
+      }
+    });
+  })
 
-  // it.only('Should exclude specific accessibility rules', () => {
-  //   cy.analyseA11y('https://example.cypress.io', {
-  //     rules: {
-  //       'color-contrast': { enabled: false }
-  //     }
-  //   });
-  // })
+  it('Should ONLY include rules with these levels of conformance', () => {
+    cy.analyseA11y('https://example.cypress.io', null, {
+      runOnly: {
+          type: 'tag',
+          values: ['wcag2a', 'wcag2aa']
+        }
+      }
+    );
+  })
 })
