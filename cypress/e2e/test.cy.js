0f37e5a058a56413da6d88ed506e93f3e7d4ebfc
Guilherme Almeida <guisalmeida.dev@gmail.com>
2022-10-17 21:54:39 -0300

    refactor: public interface

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,29 +1,29 @@
 /// <reference types="cypress" />
 
-describe('Example tests', () => {  
-  it('Should log any accessibility failures', () => {
-    cy.analyseA11y('https://example.cypress.io');
-  })
+describe("Example tests", () => {
+  // it("Should log any accessibility failures", () => {
+  //   cy.analyseA11y("/");
+  // });
 
-  it('Should execute ONLY specific elements on the page', () => {
-    cy.analyseA11y('https://example.cypress.io', '.container', null);
-  })
-  
-  it('Should exclude specific elements on the page', () => {
-    cy.analyseA11y('https://example.cypress.io', { exclude: ['.banner'] }, null);
-  })
+  // it('Should execute ONLY specific elements on the page', () => {
+  //   cy.analyseA11y('https://example.cypress.io', '.container', null);
+  // })
 
-  it('Should ONLY include rules with serious and critical impacts', () => {
-    cy.analyseA11y('https://example.cypress.io', null, { includedImpacts: ['critical', 'serious'] });
-  })
+  // it('Should exclude specific elements on the page', () => {
+  //   cy.analyseA11y('https://example.cypress.io', { exclude: ['.banner'] }, null);
+  // })
 
-  it('Should exclude specific accessibility rules', () => {
-    cy.analyseA11y('https://example.cypress.io', null, {
-      rules: {
-        'color-contrast': { enabled: false }
-      }
-    });
-  })
+  // it('Should ONLY include rules with serious and critical impacts', () => {
+  //   cy.analyseA11y('https://example.cypress.io', null, { includedImpacts: ['critical', 'serious'] });
+  // })
+
+  // it('Should exclude specific accessibility rules', () => {
+  //   cy.analyseA11y('https://example.cypress.io', null, {
+  //     rules: {
+  //       'color-contrast': { enabled: false }
+  //     }
+  //   });
+  // })
 
   it('Should ONLY include rules with these levels of conformance', () => {
     cy.analyseA11y('https://example.cypress.io', null, {
@@ -34,4 +34,4 @@ describe('Example tests', () => {
       }
     );
   })
-})
+});
